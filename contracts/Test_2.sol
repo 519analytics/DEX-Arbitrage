e66acb4af9c055d4c448041e7f84b3e8b4ba33dd
Remix Workspace <>
2022-10-27 22:55:04 -0400

    updated test 1049p

--- contracts/Test_2.sol
+++ contracts/Test_2.sol
@@ -1,5 +1,5 @@
 // test 10-27-2022
-// this better
+// this better work 10:49pm
 // this is the update 2 Test2.sol
 
 
--- contracts/test.sol
+++ contracts/test.sol
@@ -1,2 +1,3 @@
 // test 10-27-2022
-// this better work!
+// this better work! 10:49pm
+
